2622760d925ff891831cac338758cd7bf727871c
knightiti <ktess2008@gmail.com>
2021-08-18 15:06:06 -0700

    <A program that passes all betty checks>

---
+++ contributors.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Contributors to shell_h
+ * Return: 0
+ */
+
+int  main(void)
+{
+printf("Contributors to this simple_shell project include:\n");
+printf("1. Billie Mushongwi, Male, Zimbabwean.\n");
+printf("2. Knightess Oyibo, Female, Nigerian.\n");
+return (0);
+}
